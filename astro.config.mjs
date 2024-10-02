fe189fc82fc831b8f4272d2d4b962e059b5411d7
celestegambardella <celestegambardella@users.noreply.github.com>
2024-10-02 17:44:00 +0000

    deploy: 0c2ccecf3b55a6041c069a1597112ae7e7453b9d

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  // Use process.env.BASE_PATH as set by GitHub Secrets
-  base: process.env.BASE_PATH || '/',
-  
-  // Set other options as needed
-  site: 'https://celestegambardella.github.io', // Replace with your GitHub Pages URL
-});
